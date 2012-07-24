5b83652374ff9c9236e6a6b058405a2270628019
Ned Ruggeri <ruggeri@self-loop.com>
2012-07-24 07:35:46 -0700

    improve xml_document solution

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -6,33 +6,27 @@ class XmlDocument
     self.indent_level = 0
   end
   
-  def render_tag(tag_name, attrs = {}, &block)
-    xml = nil
-    
+  def render_tag(tag_name, attrs = {}, &block)    
     attrs_string = attrs.map do |key, value|
       "#{key}='#{value}'"
     end.join(" ")
     
-    elements = []
     if block
-      indent_level.times { elements << "  " } if self.indents
-      elements << (attrs.size == 0 ? "<#{tag_name}>" : "<#{tag_name} #{attrs_string}>")
-      elements << "\n" if self.indents
+      open_tag = ("  " * indent_level) + (attrs.size == 0 ? "<#{tag_name}>" :
+        "<#{tag_name} #{attrs_string}>") + (self.indents ? "\n" : "")
 
-      self.indent_level += 1
-      elements << yield
-      self.indent_level -=1
-
-      indent_level.times { elements << "  " } if self.indents
-      elements << "</#{tag_name}>"
-      elements << "\n" if self.indents
+      self.indent_level += 1 if self.indents
+      body = yield
+      self.indent_level -= 1 if self.indents
+      
+      close_tag = (" " * indent_level) + "</#{tag_name}>" + (self.indents ?
+        "\n" : "")
+      
+      [open_tag, body, close_tag].join
     else
-      indent_level.times { elements << "  " } if self.indents
-      elements << (attrs.size == 0 ? "<#{tag_name}/>" : "<#{tag_name} #{attrs_string}/>")
-      elements << "\n" if self.indents
+      ("  " * indent_level) + (attrs.size == 0 ? "<#{tag_name}/>" :
+        "<#{tag_name} #{attrs_string}/>") + (self.indents ? "\n" : "")
     end
-
-    elements.join
   end
   
   def method_missing(method_name, *args, &block)
